9fe7c94ab8fd153e8865481fc1b4e70c488669b1
yuefang <yuefang@tesla.com>
2022-03-23 20:44:58 +0800

    :bug:

--- packages/DEMO/next.config.js
+++ packages/DEMO/next.config.js
@@ -95,6 +95,23 @@ module.exports = withBundleAnalyzer(
           future: {
             webpack5: true
           },
+          async headers() {
+            return [
+              {
+                source: '/(.*)',
+                headers: [
+                  {
+                    key: 'Cross-Origin-Embedder-Policy',
+                    value: 'require-corp'
+                  },
+                  {
+                    key: 'Cross-Origin-Opener-Policy',
+                    value: 'same-origin'
+                  }
+                ]
+              }
+            ]
+          },
           esModule: true,
           // async redirects() {
           //   return [
